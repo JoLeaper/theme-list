e47e7579bde23703dd49b1a9ceaf6e0ecddb81f2
JoLeaper <joleap123@gmail.com>
2020-08-03 13:41:01 -0700

    initial commit and file structuring

---
+++ src/components/Character/Character.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import PropTypes from 'prop-types'
+
+function Character(props) {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+Character.propTypes = {
+
+}
+
+export default Character
+
---
+++ src/components/CharacterList/CharacterList.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import PropTypes from 'prop-types'
+
+function CharacterList(props) {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+CharacterList.propTypes = {
+
+}
+
+export default CharacterList
+
